a2c9d36d0cd14f9821b4be1120db6d6f5d7509c0
Arun Verma <arun.verma@sidusa.com>
2014-05-07 16:58:26 +0530

    Modified image & video upload

--- recipe_dine/app/models/procedural_step.rb
+++ recipe_dine/app/models/procedural_step.rb
@@ -2,20 +2,16 @@ class ProceduralStep < ActiveRecord::Base
 
 	belongs_to :recipe, :inverse_of => :procedural_steps
 	
-	has_attached_file :media_file, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :media_file, :content_type => ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'],
-     :message => "Invalid image format",
-     :if => :is_type_of_image?
-  validates_attachment_content_type :media_file, :content_type => ['video/x-msvideo','video/avi','video/quicktime','video/3gpp','video/x-ms-wmv','video/mp4','video/mpeg','video/x-flv', 'application/octet-stream'],
-     :message => "Invalid video format",
-     :if => :is_type_of_video?
+	has_attached_file :media_file, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :if => lambda { |attachment| attachment.instance.is_image? }
+  has_attached_file :media_file, :if => lambda { |attachment| attachment.instance.is_video? }
+  validates_attachment_content_type :media_file, :content_type => ['image/png', 'image/jpeg', 'image/jpg', 'image/gif', 'video/x-msvideo','video/avi','video/quicktime','video/3gpp','video/x-ms-wmv','video/mp4','video/mpeg','video/x-flv', 'application/octet-stream'],
+     :message => "Invalid image format"
 
-  def is_type_of_video?
-    false
+  def is_image?
+    self.media_file_content_type.include?("image")
   end
 
-  def is_type_of_image?
-    true
-  end
-
+  def is_video?
+    self.media_file_content_type.include?("video")
+  end  
 end
